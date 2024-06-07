7a299ee8baf8a3330f2ca94ae2faa54a2991f885
Sergio59 <SergioBr59CTR@gmail.com>
2024-06-07 10:42:46 -0300

    Produto model

---
+++ Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿namespace CardapioWeb.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao_curta { get; set; }
+
+        public string Descricao_Detalhada { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string Local_Imagem { get; set;}
+
+        public bool Produto_Preferido { get; set; }
+
+        public bool Em_Estoque { get; set; }
+    }
+}
